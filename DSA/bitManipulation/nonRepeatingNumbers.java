e4b0067318512c8123bb2fc078fd32497caddb44
Arjun-93 <arjun20178@iiitd.ac.in>
2023-03-27 13:24:02 +0530

    2 Questions Completed

---
+++ DSA/bitManipulation/nonRepeatingNumbers.java
@@ -0,0 +1,25 @@
+package DSA.bitManipulation;
+
+public class nonRepeatingNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,2,1,4};
+        int[] ans = singleNumber(arr);
+        System.out.println(ans[0]);
+        System.out.println(ans[1]);
+
+    }
+    public static int[] singleNumber(int[] nums){
+        int[] arr = new int[2];
+        int xorr = nums[0];
+        int j = 0;
+        for(int i=1; i<nums.length; i++){
+            xorr ^= nums[i];
+            System.out.println(xorr);
+            if(xorr== 1){
+                arr[j++] = nums[i];
+                xorr = nums[i+1];
+            }
+        }
+        return arr;
+    }
+}
