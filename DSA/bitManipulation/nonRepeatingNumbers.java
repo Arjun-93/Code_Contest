5bed467f90c28f5a9e821d92765196fc58389654
Arjun-93 <arjun20178@iiitd.ac.in>
2023-03-27 16:09:01 +0530

    Zeeny Updated

--- DSA/bitManipulation/nonRepeatingNumbers.java
+++ DSA/bitManipulation/nonRepeatingNumbers.java
@@ -1,5 +1,10 @@
 package DSA.bitManipulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.print.attribute.HashAttributeSet;
+
 public class nonRepeatingNumbers {
     public static void main(String[] args) {
         int[] arr = {1,2,3,2,1,4};
@@ -15,11 +20,24 @@ public class nonRepeatingNumbers {
         for(int i=1; i<nums.length; i++){
             xorr ^= nums[i];
             System.out.println(xorr);
-            if(xorr== 1){
+            if(xorr == 1){
                 arr[j++] = nums[i];
                 xorr = nums[i+1];
             }
         }
+        return arr;
+    }
+
+    public static int[] findSingleNumber(int[] nums){
+        int[] arr = new int[2];
+
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        int count =1;
+        for (int i =0; i<nums.length; i++){
+            if()
+        }
+
+
         return arr;
     }
 }
