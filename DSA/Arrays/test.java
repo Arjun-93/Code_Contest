67b7672b580d942047dd55c68a48cf953249bb24
Arjun-93 <arjun20178@iiitd.ac.in>
2023-06-10 10:29:48 +0530

    reversen n

--- DSA/Arrays/test.java
+++ DSA/Arrays/test.java
@@ -3,7 +3,17 @@ package DSA.Arrays;
 public class test {
     public static void main(String[] args) {
         String  st = "1101";
+        int n = 11001;
+        // String temp = Integer.toString(n);
+        // String temp1 =
         int  temp = Integer.parseInt(st, 2);
         System.out.println(temp);
+    }
+
+    public String binarySum(String a, String b){
+        long  temp1 = Long.parseLong(a, 2);
+        long  temp2 = Long.parseLong(b, 2);
+        long ans = temp1 + temp2;
+        return Long.toBinaryString(ans);
     }
 }
