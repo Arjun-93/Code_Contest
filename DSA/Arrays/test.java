9c7b186ef474b0eb91024e3bbf14eab6b5e90d31
Arjun-93 <arjun20178@iiitd.ac.in>
2023-06-10 09:52:19 +0530

    new Changes

--- DSA/Arrays/test.java
+++ DSA/Arrays/test.java
@@ -1,5 +1,9 @@
 package DSA.Arrays;
 
 public class test {
-    
+    public static void main(String[] args) {
+        String  st = "1101";
+        int  temp = Integer.parseInt(st, 2);
+        System.out.println(temp);
+    }
 }
